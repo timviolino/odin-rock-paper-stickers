350cc10a2455643a6ab860a3e143bce4e44d4b18
timviolino <timviolino@gmail.com>
2022-05-28 13:40:16 -0400

    Add basic UI

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const data = ['rock', 'paper', 'stickers'];
+var playerScore = 0;
+var computerScore = 0;
+initButtons();
+
+function initButtons() {
+  btns = document.querySelectorAll('button');
+  function addBtnEvents(btn) {
+    btn.addEventListener('click', function (){
+      playRound(btn.id, computerPlay());
+    });
+  }
+  btns.forEach(addBtnEvents);
+}
 
 function computerPlay() {
   return data[Math.floor(Math.random()*3)];
@@ -9,31 +22,37 @@ function playRound(playerSelection, computerSelection) {
   cs = computerSelection.toLowerCase();
   var i = data.indexOf(ps);
   var j = i-1 > 0 ? i-1 : data.length-1;
+  var result = '';
   if (ps == cs) {
-    return "You tied..."; 
+    result = "You tied..."; 
   }
   else if (data[j] == cs) {
-    return "You win " + ps + " beats " + cs;
+    result = "You win " + ps + " beats " + cs;
   }
-  return "You lose " + cs + " beats " + ps;
+  else {
+    result = "You lose " + cs + " beats " + ps;
+  }
+  var p = document.getElementById('results');
+  p.textContent = result;
+  updateScore(result);
 }
 
-function game() {
-  var playerScore = 0;
-  var computerScore = 0;
-  for (let i = 1; i <= 5; i++) {
-    console.log("Round " + i);
-    const playerSelection = prompt("What is your selection?/n");
-    const computerSelection = computerPlay();
-    var result = playRound(playerSelection, computerSelection);
-    console.log(result);
-    if (result.indexOf('win') >= 0) {
-      playerScore += 1;
-    }
-    else if (result.indexOf('lose') >= 0) {
-      computerScore += 1;
-    }
+function updateScore(result) {
+  score = document.getElementById('score');
+  if (result.indexOf('win') >= 0) {
+    playerScore += 1;
   }
-  console.log("Computer won " + computerScore + " time(s)!");
-  console.log("You won " + playerScore + " time(s)!");
+  else if (result.indexOf('lose') >= 0) {
+    computerScore += 1;
+  }
+  score.textContent = "Your score: " + playerScore + ", Computer's score: " +   computerScore; 
+  if (playerScore == 5){
+    score.textContent = "Yeehaw partner, you won!";
+  }
+  else if (computerScore == 5) {
+    score.textContent = "Uh oh, Computer won the match...";
+  }
+  
+  
+
 }
